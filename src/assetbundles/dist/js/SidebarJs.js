dc9aeff366579d57568a669ab72e5adb4365744f
Linnea <linneahartsuyker@imarc.com>
2023-09-14 16:54:41 -0400

    Fixing occasional null error

--- src/assetbundles/dist/js/SidebarJs.js
+++ src/assetbundles/dist/js/SidebarJs.js
@@ -30,14 +30,6 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 accordionItemHeaders.forEach(accordionItemHeader => {
   accordionItemHeader.addEventListener("click", event => {
     
-    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-    // }
-
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
     if(accordionItemHeader.classList.contains("active")) {
